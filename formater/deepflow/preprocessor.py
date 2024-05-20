69e23a3cc7e6cb3249e5e72d4332af6a4c76f1bb
Robert Gallas <gallas.robert@gmail.com>
2024-05-20 21:10:39 +0200

    peformance update and basi tests

--- formater/deepflow/preprocessor.py
+++ formater/deepflow/preprocessor.py
@@ -3,63 +3,31 @@ import hashlib
 import copy
 from collections import OrderedDict
 
-json_string = '''
-{
-      "name": "John",
-      "privateAddress": {
-          "townName": "London",
-          "__meta_id__": 1876631416,
-          "__meta_type__": "com.github.gabert.deepflow.serializer.SerMain$Address"
-      },
-      "businessAddresses": {
-          "__meta_id__": 1225616405,
-          "__meta_type__": "java.util.ArrayList",
-          "__meta_array__": [
-              {
-                  "townName": "Paris",
-                  "__meta_id__": 184966243,
-                  "__meta_type__": "com.github.gabert.deepflow.serializer.SerMain$Address"
-              },
-              {
-                  "townName": "New York",
-                  "__meta_id__": 124313277,
-                  "__meta_type__": "com.github.gabert.deepflow.serializer.SerMain$Address"
-              }
-          ]
-      },
-      "__meta_id__": 2101842856,
-      "__meta_type__": "com.github.gabert.deepflow.serializer.SerMain$Person"
-  }
-'''
-
 
 def compute_hash(obj):
-    sorted_obj = copy.deepcopy(obj)
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            obj[key] = compute_hash(value)
 
-    if isinstance(sorted_obj, dict):
-        for key, value in sorted_obj.items():
-            sorted_obj[key] = compute_hash(value)
+        obj = OrderedDict(sorted(obj.items()))
 
-        sorted_obj = OrderedDict(sorted(sorted_obj.items()))
+        hash_object = hashlib.md5(json.dumps(obj, sort_keys=True).encode())
+        obj['__hash__'] = hash_object.hexdigest()
 
-        hash_object = hashlib.md5(json.dumps(sorted_obj, sort_keys=True).encode())
-        sorted_obj['__hash__'] = hash_object.hexdigest()
+    elif isinstance(obj, list):
+        obj = [compute_hash(item) for item in obj]
 
-    elif isinstance(sorted_obj, list):
-        sorted_obj = [compute_hash(item) for item in sorted_obj]
-
-    return sorted_obj
+    return obj
 
 
 def extract_data(obj):
     if isinstance(obj, dict):
-        copy_obj = copy.deepcopy(obj)
-        for key in list(copy_obj.keys()):
+        for key in list(obj.keys()):
             if key == "__meta_id__" or key == "__meta_type__":  # Remove meta keys
-                del copy_obj[key]
+                del obj[key]
             else:  # Process nested dictionaries
-                copy_obj[key] = extract_data(copy_obj[key])
-        return copy_obj
+                obj[key] = extract_data(obj[key])
+        return obj
     elif isinstance(obj, list):  # Process list items
         return [extract_data(item) for item in obj]
     else:
@@ -68,13 +36,12 @@ def extract_data(obj):
 
 def extract_meta(obj):
     if isinstance(obj, dict):
-        copy_obj = copy.deepcopy(obj)
-        for key in list(copy_obj.keys()):
-            if key != "__meta_id__" and key != "__meta_type__" and not isinstance(copy_obj[key], (dict, list)):
-                del copy_obj[key]
+        for key in list(obj.keys()):
+            if key != "__meta_id__" and key != "__meta_type__" and not isinstance(obj[key], (dict, list)):
+                del obj[key]
             else:
-                copy_obj[key] = extract_meta(copy_obj[key])
-        return copy_obj
+                obj[key] = extract_meta(obj[key])
+        return obj
     elif isinstance(obj, list):  # Process list items
         return [extract_meta(item) for item in obj]
     else:
@@ -82,17 +49,14 @@ def extract_meta(obj):
 
 
 def merge_json(hash_data, hash_meta):
-    return _merge_internal(copy.deepcopy(hash_data), copy.deepcopy(hash_meta))
-
-
-def _merge_internal(hash_data, hash_meta):
     if isinstance(hash_data, dict):
         for key in hash_data.keys():
             if isinstance(hash_data[key], (dict, list)):
                 hash_data[key] = merge_json(hash_data[key], hash_meta[key])
             else:
                 if key == '__hash__':
-                    hash_data["__meta_id__"] = f"ID:{hash_meta['__meta_id__']}-{hash_data['__hash__']}-{hash_meta['__hash__']}"
+                    hash_data[
+                        "__meta_id__"] = f"ID:{hash_meta['__meta_id__']}-{hash_data['__hash__']}-{hash_meta['__hash__']}"
                     hash_data["__meta_type__"] = hash_meta['__meta_type__']
                     del hash_data["__hash__"]
     elif isinstance(hash_data, list):
@@ -103,52 +67,11 @@ def _merge_internal(hash_data, hash_meta):
 
 
 def hash_update(json_str):
-    json_input = json.loads(json_string)
-    json_data = extract_data(json_input)
-    json_meta = extract_meta(json_input)
-    json_hash_data = compute_hash(json_data)
-    json_hash_meta = compute_hash(json_meta)
-    json_data_merged = merge_json(json_hash_data, json_hash_meta)
+    json_input = json.loads(json_str)
+    json_data = extract_data(copy.deepcopy(json_input))
+    json_meta = extract_meta(copy.deepcopy(json_input))
+    json_hash_data = compute_hash(copy.deepcopy(json_data))
+    json_hash_meta = compute_hash(copy.deepcopy(json_meta))
+    json_data_merged = merge_json(copy.deepcopy(json_hash_data), copy.deepcopy(json_hash_meta))
 
-    # print(json.dumps(json_data_input, indent=4, sort_keys=True))
-    # print('--------------------')
-    # print(json.dumps(json_data_raw, indent=4, sort_keys=True))
-    # print('--------------------')
-    # print(json.dumps(json_data_meta, indent=4, sort_keys=True))
-    print('--------------------')
-    print(json.dumps(json_hash_data, indent=4, sort_keys=True))
-    print('--------------------')
-    print(json.dumps(json_hash_meta, indent=4, sort_keys=True))
-    print('--------------------')
-    print(json.dumps(json_data_merged, indent=4, sort_keys=True))
-
-
-if __name__ == '__main__':
-    hash_update(json_string)
-
-    # print(extract_data(json_string))
-    # print(extract_metadata(json_string))
-
-    # data = json.loads(json_string)
-    # data2 = json.loads(json_string_2)
-    # data3 = json.loads(json_string_3)
-    # data4 = json.loads(json_string_4)
-    # output_meta = extract_meta(data)
-    # output_data = extract_data(data)
-    # output_data_2 = extract_data(data2)
-    # output_data_3 = extract_data(data3)
-    # output_data_4 = extract_data(data4)
-
-    # print(json.dumps(data, indent=4, sort_keys=True))
-    # print("1. ------------")
-    # print(json.dumps(output_meta, indent=4, sort_keys=True))
-    # print("2. ------------")
-    # print(json.dumps(output_data, indent=4, sort_keys=True))
-    # print("3. ------------")
-    # print(json.dumps(compute_hash(output_data), indent=4, sort_keys=True))
-    # print("4. ------------")
-    # print(json.dumps(compute_hash(output_data_2), indent=4, sort_keys=True))
-    # print("5. ------------")
-    # print(json.dumps(compute_hash(output_data_3), indent=4, sort_keys=True))
-    # print("6. ------------")
-    # print(json.dumps(compute_hash(output_data_4), indent=4, sort_keys=True))
+    return json_data_merged
--- formater/tests/preprocessor_test.py
+++ formater/tests/preprocessor_test.py
@@ -1,7 +1,77 @@
 import unittest
-from deepflow.preprocessor import extract_meta
 
-class TestPreprocessor(unittest.TestCase):
-    def test_extract_meta(self):
-        pass
+from deepflow.preprocessor import compute_hash, hash_update
 
+json_test_string = '''
+{
+      "name": "John",
+      "privateAddress": {
+          "townName": "London",
+          "__meta_id__": 1876631416,
+          "__meta_type__": "com.github.gabert.deepflow.serializer.Main$Address"
+      },
+      "businessAddresses": {
+          "__meta_id__": 1225616405,
+          "__meta_type__": "java.util.ArrayList",
+          "__meta_array__": [
+              {
+                  "townName": "Paris",
+                  "__meta_id__": 184966243,
+                  "__meta_type__": "com.github.gabert.deepflow.serializer.Main$Address"
+              },
+              {
+                  "townName": "New York",
+                  "__meta_id__": 124313277,
+                  "__meta_type__": "com.github.gabert.deepflow.serializer.Main$Address"
+              }
+          ]
+      },
+      "__meta_id__": 2101842856,
+      "__meta_type__": "com.github.gabert.deepflow.serializer.Main$Person"
+  }
+'''
+
+
+class TestComputeHash(unittest.TestCase):
+    def test_compute_hash_dictionary(self):
+        test_dict = {'a': 1,
+                     'b': {'c': 2},
+                     'd': {
+                         '__meta_array__':
+                             [1,
+                              {'x': 11,
+                               'y': 12},
+                              3]
+                     }}
+        result = compute_hash(test_dict)
+        self.assertIn('__hash__', result)
+        self.assertIn('__hash__', result['b'])
+        self.assertIn('__hash__', result['d'])
+        self.assertIn('__hash__', result['d']['__meta_array__'][1])
+
+        self.assertNotEqual(result['__hash__'],
+                            result['b']['__hash__'])
+        self.assertNotEqual(result['b']['__hash__'],
+                            result['d']['__hash__'])
+        self.assertNotEqual(result['d']['__hash__'],
+                            result['d']['__meta_array__'][1]['__hash__'])
+
+
+class TestComputeJsonHashUpdate(unittest.TestCase):
+    def test_compute_json_hash_update(self):
+        result = hash_update(json_test_string)
+
+        self.assertIn('__meta_id__', result)
+        self.assertIn('__meta_id__', result['privateAddress'])
+        self.assertIn('__meta_id__', result['businessAddresses'])
+        self.assertIn('__meta_id__', result['businessAddresses']['__meta_array__'][0])
+        self.assertIn('__meta_id__', result['businessAddresses']['__meta_array__'][1])
+
+        self.assertNotIn('__hash__', result)
+        self.assertNotIn('__hash__', result['privateAddress'])
+        self.assertNotIn('__hash__', result['businessAddresses'])
+        self.assertNotIn('__hash__', result['businessAddresses']['__meta_array__'][0])
+        self.assertNotIn('__hash__', result['businessAddresses']['__meta_array__'][1])
+
+        self.assertEqual(2, len(result['__meta_id__'].split(":")))
+        self.assertEqual(3, len(result['__meta_id__'].split("-")))
