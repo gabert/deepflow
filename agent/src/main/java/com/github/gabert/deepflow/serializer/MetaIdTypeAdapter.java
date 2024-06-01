d045e2c40b4f31cf2c143e13210c3b88d3d4b161
Robert Gallas <gallas.robert@gmail.com>
2024-06-01 15:05:28 +0200

    serialization format fix

--- agent/src/main/java/com/github/gabert/deepflow/serializer/MetaIdTypeAdapter.java
+++ agent/src/main/java/com/github/gabert/deepflow/serializer/MetaIdTypeAdapter.java
@@ -1,5 +1,6 @@
 package com.github.gabert.deepflow.serializer;
 
+import com.github.gabert.deepflow.agent.DeepFlowAdvice;
 import com.google.gson.*;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonReader;
@@ -18,22 +19,25 @@ public class MetaIdTypeAdapter<T> extends TypeAdapter<T> {
     public void write(JsonWriter out, T value) throws IOException {
         JsonElement jsonElement = defaultAdapter.toJsonTree(value);
 
+        Class<?> valueClass = value.getClass();
+        String className = DeepFlowAdvice.formatClassName(valueClass);
+
         if (jsonElement.isJsonObject()) {
             JsonObject jsonObject = jsonElement.getAsJsonObject();
             int systemId = System.identityHashCode(value);
-            jsonObject.addProperty("__type__", value.getClass().getName());
+            jsonObject.addProperty("__type__", className);
             jsonObject.addProperty("__id__", systemId);
         } else if (jsonElement.isJsonArray()) {
             JsonObject jsonObject = new JsonObject();
             int systemId = System.identityHashCode(value);
-            jsonObject.addProperty("__type__", value.getClass().getName());
+            jsonObject.addProperty("__type__", className);
             jsonObject.addProperty("__id__", systemId);
             jsonObject.add("__value__", jsonElement);
             jsonElement = jsonObject;
         } else if (jsonElement.isJsonPrimitive()) {
             JsonObject jsonObject = new JsonObject();
             int systemId = System.identityHashCode(value);
-            jsonObject.addProperty("__type__", value.getClass().getName());
+            jsonObject.addProperty("__type__", className);
             jsonObject.addProperty("__id__", systemId);
             jsonObject.add("__value__", jsonElement);
             jsonElement = jsonObject;
