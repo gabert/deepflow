b30e33ac6576734e84a6f25eed3a567fff17dd20
Robert Gallas <gallas.robert@gmail.com>
2024-06-01 09:43:40 +0200

    Formater skeleton.

---
+++ formater/deepflow/line_formater.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+import yaml
+
+
+class BaseFormater(ABC):
+    @abstractmethod
+    def format(self, data):
+        pass
+
+
+class YamlLineFormater(BaseFormater):
+    def __init__(self):
+        self.indent = 4
+        self.previous_level = -1
+        self.record_formats = {
+            "MS": lambda record: self.format_ms(record, self.previous_level),
+            # "TS": lambda record: format_ts(record, 'TS'),
+            # "TE": lambda record: format_ts(record, 'TE'),
+            # "AR": lambda record: format_ar(record),
+            # "RE": lambda record: format_re(record)
+        }
+
+    def format(self, record):
+        yaml_lines = self.record_formats.get(record["type"], lambda _: [])(record)
+        return yaml_lines
+
+    def format_ms(self, record, previous_level):
+        data = record['value']
+
+        indent_size = self.calculate_indent(record['type'], record['depth'])
+        indent_string = ' ' * indent_size
+        method_indent_string = (' ' * self.indent).replace(' ', '-', 1)
+
+        yaml_lines = []
+
+        if record['depth'] > previous_level:
+            yaml_lines.append(indent_string + 'CS:')
+
+        yaml_lines.append(f"{indent_string}{method_indent_string}'{data}':")
+
+        return yaml_lines
+
+    def calculate_indent(self, record_type, depth):
+        if record_type == 'MS':
+            return depth * (2 * self.indent)
+        else:
+            return (depth + 1) * (2 * self.indent)
