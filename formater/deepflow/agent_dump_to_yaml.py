bd4f8229d5cf7336bf592aad1424c26dbd379a18
Robert Gallas <gallas.robert@gmail.com>
2024-06-02 07:16:22 +0200

    Formater factory method introduction

--- formater/deepflow/agent_dump_to_yaml.py
+++ formater/deepflow/agent_dump_to_yaml.py
@@ -1,10 +1,9 @@
-import yaml
 import os
 
 from deepflow import preprocessor, metadata_strip
+from deepflow.line_formater import FormaterFactory
 
 delimiter = ";"
-indent = 4
 
 
 def process_session(directory):
@@ -18,13 +17,13 @@ def process_session(directory):
 
 
 def process_dump_file(dump_file_path, yaml_file_path):
-    yaml_file = open_file(yaml_file_path, 'a')
+    yaml_file = open_file(yaml_file_path, 'w')
     dump_file = open_file(dump_file_path)
 
-    previous_level = -1
+    base_formater = FormaterFactory.get_formater('yaml')
 
     for dump_line in dump_file:
-        yaml_entries, previous_level = process_dump_line(dump_line.rstrip('\n'), previous_level)
+        yaml_entries = process_dump_line(dump_line.rstrip('\n'), base_formater)
         for entry in yaml_entries:
             yaml_file.write(f'{entry}\n')
 
@@ -32,114 +31,39 @@ def process_dump_file(dump_file_path, yaml_file_path):
     yaml_file.close()
 
 
-def process_dump_line(line, previous_level):
-    record_formats = {
-        "MS": lambda rec: format_ms(rec, previous_level),
-        "TS": lambda rec: format_ts(rec, 'TS'),
-        "TE": lambda rec: format_ts(rec, 'TE'),
-        "AR": lambda rec: format_ar(rec),
-        "RE": lambda rec: format_re(rec)
-    }
+def process_dump_line(line, base_formater):
+    record = parse_line(line)
 
-    fields = split_line(line)
-    record = parse_fields(fields)
-    yaml_entries = record_formats.get(record["type"], lambda _: [])(record)
+    if record['type'] == "AR" or record['type'] == "RE":
+        record = compute_hash(record)
 
-    return yaml_entries, record['depth']
+    file_entries = base_formater.format(record)
+
+    return file_entries
 
 
-def split_line(line):
+def parse_line(line):
     parts = line.split(delimiter)
-    depth = parts[0]
+    depth = int(parts[0])
     thread = parts[1]
     record_type = parts[2]
     value = ";".join(parts[3:])
 
-    return depth, thread, record_type, value
-
-
-def parse_fields(fields):
     return {
-        "depth": int(fields[0]),
-        "thread": fields[1],
-        "type": fields[2],
-        "value": fields[3]
+        "depth": depth,
+        "thread": thread,
+        "type": record_type,
+        "value": value
     }
 
 
-# def parse_fields_ts(fields):
-#     return {
-#         "depth": int(fields[0]),
-#         "thread": fields[1],
-#         "type": fields[2],
-#         "value": fields[3]
-#     }
-
-
-def format_ms(record, previous_level):
-    data = record['value']
-    offset_string = ' ' * indent_size(record['type'], record['depth'])
-    yaml_lines = []
-
-    if record['depth'] > previous_level:
-        yaml_lines.append(offset_string + 'CS:')
-
-    yaml_lines.append(f"{offset_string}{(' ' * indent).replace(' ', '-', 1)}'{data}':")
-    return yaml_lines
-
-
-def format_ts(record, tag):
-    data = record['value']
-    offset_string = ' ' * indent_size(record['type'], record['depth'])
-    yaml_lines = []
-    yaml_lines.append(f"{offset_string}{tag}: '{data}'")
-    return yaml_lines
-
-
-def format_ar(record):
+def compute_hash(record):
     json_string = record['value']
     data_hashed = preprocessor.hash_update(json_string)
-    raw_data = metadata_strip.extract_data(data_hashed)
-    meta_data = metadata_strip.extract_metadata(data_hashed)
+    record['raw_data'] = metadata_strip.extract_data(data_hashed)
+    record['meta_data'] = metadata_strip.extract_metadata(data_hashed)
 
-    raw_data_yaml_lines = convert_to_yaml(raw_data, "AR", record['type'], record['depth'])
-    meta_data_yaml_lines = convert_to_yaml(meta_data, "AI", record['type'], record['depth'])
-
-    yaml_lines = raw_data_yaml_lines + meta_data_yaml_lines
-
-    return yaml_lines
-
-
-def convert_to_yaml(data, tag, record_type, depth):
-    tagged_object = {tag: data}
-    yaml_string = yaml.dump(tagged_object, indent=indent, sort_keys=True).rstrip('\n')
-
-    offset_string = ' ' * indent_size(record_type, depth)
-    yaml_lines = []
-    for yaml_line in yaml_string.split("\n"):
-        yaml_lines.append(f'{offset_string}{yaml_line}')
-    return yaml_lines
-
-
-def format_re(record):
-    json_string = record['value']
-    data_hashed = preprocessor.hash_update(json_string)
-    raw_data = metadata_strip.extract_data(data_hashed)
-    meta_data = metadata_strip.extract_metadata(data_hashed)
-
-    raw_data_yaml_lines = convert_to_yaml(raw_data, "RE", record['type'], record['depth'])
-    meta_data_yaml_lines = convert_to_yaml(meta_data, "RI", record['type'], record['depth'])
-
-    yaml_lines = raw_data_yaml_lines + meta_data_yaml_lines
-
-    return yaml_lines
-
-
-def indent_size(record_type, depth):
-    if record_type == 'MS':
-        return depth * (2 * indent)
-    else:
-        return (depth + 1) * (2 * indent)
+    return record
 
 
 def open_file(filename, mode='r'):
--- formater/deepflow/line_formater.py
+++ formater/deepflow/line_formater.py
@@ -14,18 +14,19 @@ class YamlLineFormater(BaseFormater):
         self.indent = 4
         self.previous_level = -1
         self.record_formats = {
-            "MS": lambda record: self.__format_ms(record, self.previous_level),
-            # "TS": lambda record: format_ts(record, 'TS'),
-            # "TE": lambda record: format_ts(record, 'TE'),
-            # "AR": lambda record: format_ar(record),
-            # "RE": lambda record: format_re(record)
+            "MS": lambda record: self.__format_ms(record),
+            "TS": lambda record: self.__format_ts_te(record),
+            "TE": lambda record: self.__format_ts_te(record),
+            "AR": lambda record: self.__format_ar_re(record),
+            "RE": lambda record: self.__format_ar_re(record)
         }
 
     def format(self, record):
         yaml_lines = self.record_formats.get(record["type"], lambda _: [])(record)
+        self.previous_level = record["depth"]
         return yaml_lines
 
-    def __format_ms(self, record, previous_level):
+    def __format_ms(self, record):
         data = record['value']
 
         indent_size = self.__calculate_indent(record['type'], record['depth'])
@@ -34,11 +35,48 @@ class YamlLineFormater(BaseFormater):
 
         yaml_lines = []
 
-        if record['depth'] > previous_level:
+        if record['depth'] > self.previous_level:
             yaml_lines.append(indent_string + 'CS:')
 
         yaml_lines.append(f"{indent_string}{method_indent_string}'{data}':")
 
+        return yaml_lines
+
+    def __format_ts_te(self, record):
+        data = record['value']
+        offset_string = ' ' * self.__calculate_indent(record['type'], record['depth'])
+        yaml_lines = [f"{offset_string}{record['type']}: '{data}'"]
+        return yaml_lines
+
+    def __format_ar_re(self, record):
+        if record['type'] == "AR":
+            identity_tag = 'AI'
+        elif record['type'] == "RE":
+            identity_tag = 'RI'
+        else:
+            raise ValueError(f"Record type {record['type']} not supported")
+
+        raw_data_yaml_lines = self.__convert_to_yaml(record['raw_data'],
+                                                     record['type'],
+                                                     record['type'],
+                                                     record['depth'])
+        meta_data_yaml_lines = self.__convert_to_yaml(record['meta_data'],
+                                                      identity_tag,
+                                                      record['type'],
+                                                      record['depth'])
+
+        yaml_lines = raw_data_yaml_lines + meta_data_yaml_lines
+
+        return yaml_lines
+
+    def __convert_to_yaml(self, data, tag, record_type, depth):
+        tagged_object = {tag: data}
+        yaml_string = yaml.dump(tagged_object, indent=self.indent, sort_keys=True).rstrip('\n')
+
+        offset_string = ' ' * self.__calculate_indent(record_type, depth)
+        yaml_lines = []
+        for yaml_line in yaml_string.split("\n"):
+            yaml_lines.append(f'{offset_string}{yaml_line}')
         return yaml_lines
 
     def __calculate_indent(self, record_type, depth):
@@ -46,3 +84,16 @@ class YamlLineFormater(BaseFormater):
             return depth * (2 * self.indent)
         else:
             return (depth + 1) * (2 * self.indent)
+
+
+class FormaterFactory:
+    __formatters = {
+        'yaml': YamlLineFormater
+    }
+
+    @staticmethod
+    def get_formater(destination_format):
+        formater_class = FormaterFactory.__formatters.get(destination_format.lower())
+        if formater_class is None:
+            raise ValueError(f"Unknown format: {destination_format}")
+        return formater_class()
