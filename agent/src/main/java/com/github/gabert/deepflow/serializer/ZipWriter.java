552007232a0735c7598097981824914c93bed167
Robert Gallas <gallas.robert@gmail.com>
2024-06-03 21:07:50 +0200

    Compressing trace output implementation.

---
+++ agent/src/main/java/com/github/gabert/deepflow/serializer/ZipWriter.java
@@ -0,0 +1,68 @@
+package com.github.gabert.deepflow.serializer;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.*;
+
+public class ZipWriter implements Closeable {
+    private final ZipOutputStream zipOutputStream;
+    private final BufferedWriter writer;
+    private Boolean isClosed = Boolean.FALSE;
+
+    public ZipWriter(String zipEntryFile) throws IOException {
+        Path zipEntryFilePath = Paths.get(zipEntryFile);
+        String zipEntryName = zipEntryFilePath.getFileName().toString();
+        Path zipFilePath = zipFilePath(zipEntryFilePath);
+
+        Path parentDir = zipFilePath.toAbsolutePath().getParent();
+        if (parentDir != null && !Files.exists(parentDir)) {
+            Files.createDirectories(parentDir);
+        }
+
+        FileOutputStream fos = new FileOutputStream(zipFilePath.toAbsolutePath().toString());
+        this.zipOutputStream = new ZipOutputStream(new BufferedOutputStream(fos));
+        this.zipOutputStream.setLevel(Deflater.DEFLATED);
+        this.zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
+        this.writer = new BufferedWriter(new OutputStreamWriter(zipOutputStream));
+
+        Runtime.getRuntime().addShutdownHook(new Thread(this::close));
+    }
+
+    public void write(String line) throws IOException {
+        if (isClosed) {
+            throw new IOException("Zip file is already closed.");
+        }
+
+        try {
+            this.writer.write(line);
+            this.writer.newLine();
+            // TODO: Consider flushing periodicity
+            this.writer.flush();
+        } catch (IOException e) {
+            this.close();
+            throw e;
+        }
+    }
+
+    @Override
+    public void close()  {
+        this.isClosed = Boolean.TRUE;
+
+        try {
+            writer.flush();
+            zipOutputStream.closeEntry();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Path zipFilePath(Path filePath) {
+        Path parentDirectory = filePath.getParent();
+        String zipFileName = filePath.getFileName().toString().replaceFirst(".dft", ".dfz");
+        return parentDirectory.resolve(zipFileName);
+    }
+}
